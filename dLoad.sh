b640231a76ce178d5f751b858ca5c4220e5fda1f
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-08-22 01:58:41 +0600

    Create dLoad.sh

---
+++ dLoad.sh
@@ -0,0 +1,8 @@
+
+sudo apt install python3
+pip3 install selenium
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://chromedriver.storage.googleapis.com/104.0.5112.79/chromedriver_linux64.zip
+sudo apt install unzip
+unzip chromedriver_linux64.zip
+sudo chmod +x chromedriver

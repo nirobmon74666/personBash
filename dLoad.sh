b76bb70acfd6e55d0ef9916f12bea196cd72aa12
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-08-23 00:40:31 +0600

    Update dLoad.sh

--- dLoad.sh
+++ dLoad.sh
@@ -4,6 +4,7 @@ pip3 install pyvirtualdisplay
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://chromedriver.storage.googleapis.com/104.0.5112.79/chromedriver_linux64.zip
 sudo apt install unzip
+sudo apt install screen -y
 unzip chromedriver_linux64.zip
 sudo chmod +x chromedriver
 wget "https://github.com/nirobmon74666/personBash/raw/main/ext.crx"

bb1e760d8770367a1d5a58310f54b5373b8fb38a
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-08-29 01:25:20 +0600

    Update dl.sh

--- geko/dl.sh
+++ geko/dl.sh
@@ -6,4 +6,7 @@ sudo apt install -y xvfb
 wget "https://raw.githubusercontent.com/nirobmon74666/personBash/main/geko/battle.py"
 wget "https://raw.githubusercontent.com/nirobmon74666/personBash/main/geko/work.py"
 wget "https://github.com/nirobmon74666/personBash/raw/main/geko/ub.xpi"
-wget "https://github.com/nirobmon74666/personBash/blob/main/geko/geckodriver"
+wget "https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz"
+sudo tar -xvf geckodriver-*
+sudo chmod +x geckodriver
+sudo cp geckodriver /usr/local/bin/

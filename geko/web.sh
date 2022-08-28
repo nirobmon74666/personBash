74098db9278453f4a67fe0693b3a9a75d3249b69
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-08-29 00:47:05 +0600

    Update web.sh

--- geko/web.sh
+++ geko/web.sh
@@ -1,7 +1,2 @@
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt --fix-broken install -y
-sudo cp chromedriver /usr/lib
-sudo cp chromedriver /usr/bin
 sudo apt install -y xvfb
 pip install pyvirtualdisplay
-source piyo/bin/activate

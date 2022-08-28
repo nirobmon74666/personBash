3f4f829d19f981f037284288c4aa016c1c53c0c1
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-08-29 02:30:22 +0600

    Update dl.sh

--- geko/dl.sh
+++ geko/dl.sh
@@ -7,6 +7,8 @@ wget "https://raw.githubusercontent.com/nirobmon74666/personBash/main/geko/battl
 wget "https://raw.githubusercontent.com/nirobmon74666/personBash/main/geko/work.py"
 wget "https://github.com/nirobmon74666/personBash/raw/main/geko/ub.xpi"
 wget "https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz"
+wget "https://raw.githubusercontent.com/nirobmon74666/personBash/main/geko/resolv.conf"
+sudo cp resolv.conf /etc/
 sudo tar -xvf geckodriver-*
 sudo chmod +x geckodriver
 sudo cp geckodriver /usr/local/bin/

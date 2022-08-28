66564f6a494a10840f0afb14b9c3707fc00f3b21
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-08-29 01:02:58 +0600

    Update dl.sh

--- geko/dl.sh
+++ geko/dl.sh
@@ -1,1 +1,9 @@
-wget 
+sudo apt install python3
+sudo apt install pip
+pip install selenium
+pip install pyvirtualdisplay
+sudo apt install -y xvfb
+wget "https://raw.githubusercontent.com/nirobmon74666/personBash/main/geko/battle.py"
+wget "https://raw.githubusercontent.com/nirobmon74666/personBash/main/geko/work.py"
+wget "https://github.com/nirobmon74666/personBash/raw/main/geko/ub.xpi"
+wget "https://github.com/nirobmon74666/personBash/blob/main/geko/geckodriver"

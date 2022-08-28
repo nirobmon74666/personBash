b9d76a97a064833b32099bbc689cbabefafee25e
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2022-08-29 01:45:28 +0600

    Update battle.py

--- geko/battle.py
+++ geko/battle.py
@@ -10,7 +10,10 @@ import os
 import time
 from urllib.parse import urlparse
 
-profile= webdriver.FirefoxProfile('/root/.mozilla/firefox/*.default-release')
+with open('path.txt') as f:
+    proPath = f.read()
+    
+profile= webdriver.FirefoxProfile(proPath)
 profile.add_extension(extension='ub.xpi')
 profile.set_preference("dom.webdriver.enabled", False)
 profile.set_preference('useAutomationExtension', False)

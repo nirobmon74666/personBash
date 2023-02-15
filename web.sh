6608d99aade25e9c5b10ea994557582c2b4eac9b
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2023-02-16 03:56:21 +0600

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,6 +1,13 @@
+rm google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt --fix-broken install -y
 sudo cp chromedriver /usr/lib
+sudo chmod +x /usr/lib/chromedriver
 sudo cp chromedriver /usr/bin
+sudo chmod +x /usr/bin/chromedriver
 sudo apt install -y xvfb
-sudo apt install screen -y
+sudo apt install -y screen
+#sudo cp resolv.conf /etc/
+pip3 install pyvirtualdisplay
+#source piyo/bin/activate

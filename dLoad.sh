c45cd72678e343037d8e5598b6fce7b7c02483a8
nirobmon74666 <108065356+nirobmon74666@users.noreply.github.com>
2023-02-16 04:05:53 +0600

    Update dLoad.sh

--- dLoad.sh
+++ dLoad.sh
@@ -1,8 +1,9 @@
 sudo apt install python3
 pip3 install selenium
 pip3 install pyvirtualdisplay
+rm google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget https://chromedriver.storage.googleapis.com/104.0.5112.79/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_linux64.zip
 sudo apt install unzip
 sudo apt install screen -y
 unzip chromedriver_linux64.zip
